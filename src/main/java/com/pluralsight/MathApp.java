591a725b6d67e318d2d80a1c19a3b6c165251ddc
Dollant <ariawilliams2425@gmail.com>
2026-04-07 15:12:34 -0400

    Calculated highest salary between Bob and Gary.

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
 public class MathApp {
+    public static void main(String[] args) {
+// Question 1: Highest salary between Bob and Gary
+        int bobSalary = 55000;
+        int garySalary = 72000;
+        int highestSalary = Math.max(bobSalary, garySalary);
+        System.out.println("The highest salary is $" + highestSalary);
+
+
+    }
 }
