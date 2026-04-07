347a31ebef35065ea88b2b49ba702b054035fcbd
Dollant <ariawilliams2425@gmail.com>
2026-04-07 15:19:28 -0400

    Found the lowest price of vehicle using Math.min comparing truck prices versus car prices

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -8,6 +8,11 @@ public class MathApp {
         int highestSalary = Math.max(bobSalary, garySalary);
         System.out.println("The highest salary is $" + highestSalary);
 
+// Question 2: Smallest of carPrice and truckPrice
+        int carPrice = 24999;
+        int truckPrice = 38500;
+        int lowestPrice = Math.min(carPrice, truckPrice);
+        System.out.println("The lowest vehicle price is $" + lowestPrice);
 
     }
 }
